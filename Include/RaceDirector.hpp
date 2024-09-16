4036e150d6e178b8b86076bdf2f1cfa4e76062b4
Felipe Granado <31190972+felipegranado@users.noreply.github.com>
2024-09-16 14:24:38 -0300

    Update RaceDirector.hpp

--- Include/RaceDirector.hpp
+++ Include/RaceDirector.hpp
@@ -3,10 +3,23 @@
 
 #include "InternalsPlugin.hpp"     // classe base da qual os objetos de plugin devem derivar
 #include <cstdio>                  // para logging
+#include <stdio.h>                 // para saída de exemplo
+#include <math.h>                  // para arco da tangente, raiz quadrada
+
 
 class RaceDirectorPlugin : public InternalsPluginV07 // LEMBRETE: a funcao exportada GetPluginVersion() deve retornar 1 se voce estiver derivando de InternalsPluginV01, 2 para InternalsPluginV02, etc.
 {
   public:
+
+    // Construtor / Destrutor
+    ExampleInternalsPlugin() {}
+    ~ExampleInternalsPlugin() {}
+
+    void EnterRealtime();          // entering realtime
+    void ExitRealtime();           // exiting realtime
+
+    void StartSession();           // session has started
+    void EndSession();             // session has ended
 
     // SAIDA DO JOGO
     void RegistrarLimitador( const TelemInfoV01 &telem, const VehicleScoringInfoV01 &vehscoring, const ScoringInfoV01 &scoring);
@@ -14,7 +27,11 @@ class RaceDirectorPlugin : public InternalsPluginV07 // LEMBRETE: a funcao expor
   private:
 
     // ESCREVER NO LOG
-    void WriteLog( const char * const openStr, const char * const msg );
+    void EscreverLog( const char* const openStr, const char* const msg );
+
+    // necessário para o exemplo de hardware
+    double mET;
+    bool mEnabled;
 };
 
 
